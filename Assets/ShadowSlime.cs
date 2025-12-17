f1d017f789e5bc784bb671609a6073ead841b6f4
Aymen Djoudi <djoudiaymenbusiness@gmail.com>
2025-12-17 20:36:22 +0100

    made the game first enemy

--- Assets/ShadowSlime.cs
+++ Assets/ShadowSlime.cs
@@ -1,33 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShadowSlime : MonoBehaviour
 {
-    [SerializeField] private int health = 10;
-    [SerializeField] private int damage = 5;
-    // Start is called before the first frame update
+    [SerializeField]private float patrolSpeed = 2f;
+    [SerializeField]private float aggroSpeed = 4f;
+    [SerializeField]private float aggroDistance = 4f;
+    [SerializeField]private Transform player;
+    [SerializeField]private GameObject RPoint;
+    [SerializeField]private GameObject LPoint;
+    private bool playerisinfront;
+    private Rigidbody2D rb;
+    private int direction = 1; // 1 = right, -1 = left
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-        if (health <= 0)
+        PlayerInFront();
+        if (playerisinfront)
         {
-            Die();
+            AggroMove();
+        }
+        else
+        {
+            Patrol();
         }
     }
-    void Die()
+    void Patrol()
     {
-        Destroy(gameObject);
+        rb.velocity = new Vector2 (patrolSpeed * direction,rb.velocity.y);
+        if (direction == 1 && transform.position.x >= RPoint.transform.position.x)
+            {
+                Flip();
+            }
+        if (direction == -1 && transform.position.x <= LPoint.transform.position.x)
+            {
+                Flip();
+            }
+    }
+    void Flip()
+    {
+        if (transform.position.x >= RPoint.transform.position.x || transform.position.x <= LPoint.transform.position.x)
+        {
+            direction *= -1;
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x) * direction;
+            transform.localScale = scale;
+        }
+    }
+    void PlayerInFront()
+    {
+        if (Math.Abs(player.position.x - transform.position.x) <= aggroDistance)
+        {
+            playerisinfront = false;
+            return;
+        }
+        else
+        {
+            playerisinfront = true;
+        }
+    }
+    
+    void AggroMove()
+    {
+        rb.velocity = new Vector2 (aggroSpeed * direction,rb.velocity.y);
     }
     
 }
